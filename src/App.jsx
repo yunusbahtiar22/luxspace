a532e730dbbd077d76b0119b23beaf3111324f0d
yunusbahtiar22 <aboomarmoshab22@gmail.com>
2022-10-23 00:29:34 +0700

    create hero components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/header";
+import Hero from "../components/hero";
+export default function home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
